72813c4e30d21d577779607b34356a0c85ed0a54
Mike Zhou <mike.zhou@gov.bc.ca>
2026-04-28 16:18:26 -0700

    update cpf-config parse on OCP

--- config/initializers/cpf_config.rb
+++ config/initializers/cpf_config.rb
@@ -4,18 +4,21 @@ require "erb"
 
 raw = ERB.new(File.read(Rails.root.join("config", "cpf_config.yaml"))).result
 file = YAML.safe_load(raw, aliases: true) || {}
-all_configs = file["data"] || {}
-
-embedded = all_configs["cpf_config.yaml"]
-CPF_CONFIG =
-  case embedded
-  when String
-    YAML.safe_load(embedded, aliases: true) || {}
-  when Hash
-    embedded
-  else
-    {}
-  end
+if development?
+  all_configs = file["data"] || {}
+  embedded = all_configs["cpf_config.yaml"]
+  CPF_CONFIG =
+    case embedded
+    when String
+      YAML.safe_load(embedded, aliases: true) || {}
+    when Hash
+      embedded
+    else
+      {}
+    end
+else
+  CPF_CONFIG = file || {}
+end
 
 API_PROVIDERS = CPF_CONFIG["api_providers"] || {}
 WORKER_OPTIONS = CPF_CONFIG["worker_options"] || {}

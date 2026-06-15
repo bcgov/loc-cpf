c6f3fc1db54ee1caf00045111808c66e65a8685b
Mike Zhou <mike.zhou@gov.bc.ca>
2026-06-15 16:40:19 -0700

    debug header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,21 +6,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:status]
 
   def authenticate_user!
-    $stdout.puts("Headers")
-    headers = request.headers.to_h.each_with_object({}) do |(key, value), out|
-      # next unless key.start_with?("HTTP_") || %w[CONTENT_TYPE CONTENT_LENGTH].include?(key)
-
-      out[key] =
-        if key.match?(/HTTP_AUTHORIZATION|HTTP_COOKIE|HTTP_X_CSRF_TOKEN/)
-          "[FILTERED]"
-        else
-          value
-        end
-    end
-
-    msg = "[RequestHeaders] #{request.request_method} #{request.fullpath} #{headers}"
-    Rails.logger.info(msg)
-    $stdout.puts(msg)
+    Rails.logger.info request.headers.to_h.inspect
 
     
     if Rails.env.development?
